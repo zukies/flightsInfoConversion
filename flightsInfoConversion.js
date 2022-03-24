b5570d8fdb3bbf1011374b00f7f961059a3467d0
zukies <zukies28@gmail.com>
2022-03-24 14:12:21 +0200

    Update flightsInfoConversion.js

--- flightsInfoConversion.js
+++ flightsInfoConversion.js
@@ -48,29 +48,3 @@ document.querySelector("button").addEventListener("click", function () {
     }
   }
 });
-
-//////////////////////////////////////////////////////////////////////
-//This is my second try after looking at lecturers answer to this challenge,
-// I coded this without looking at his answer during the project
-//////////////////////////////////////////////////////////////////////
-
-// "use strict";
-// document.body.append(document.createElement("textarea"));
-// document.body.append(document.createElement("button"));
-// document.querySelector("button").addEventListener("click", function () {
-//   const text = document.querySelector("textarea").value;
-//   const flights = text;
-//   const goTo = (param) => param.slice(0, 3).toUpperCase();
-//   for (const flight of flights.split("+")) {
-//     const [typeOf, from, to, time] = flight.split(";");
-
-//     const type = typeOf.replaceAll("_", " ");
-//     const delayed = type.startsWith(" Delayed")
-//       ? type.replace(" ", "🔴 ")
-//       : type.replace(" ", "").padStart(20, " ");
-
-//     console.log(
-//       `${delayed} from ${goTo(from)} to ${goTo(to)} (${time.replace(":", "h")})`
-//     );
-//   }
-// });
